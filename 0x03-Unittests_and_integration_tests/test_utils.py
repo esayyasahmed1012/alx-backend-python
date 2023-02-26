6bc7a9afd38756291cd36cf0445dde31075123b0
esayyas <esayyasahmed1012@gmail.com>
2023-02-26 20:50:51 +0300

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,24 +1,21 @@
-
 #!/usr/bin/env python3
-""" Unittest module """
+""" Test SUITE Unittest module Task """
 
 from unittest import TestCase, mock
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-
 from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(TestCase):
     """ Class for testing Nested Map function """
-    # order of args: map, path, expected_output
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {'b': 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, map, path, expected_output):
-        """ Test method returns correct output """
+        """ Test method return output """
         real_output = access_nested_map(map, path)
         self.assertEqual(real_output, expected_output)
 
@@ -42,7 +39,7 @@ class TestGetJson(TestCase):
     ])
     def test_get_json(self, test_url, test_payload):
         """ Test method returns correct output """
-        # create Mock object with json method that returns test_payload
+        # set mock response to have return value of test payload
         mock_response = Mock()
         mock_response.json.return_value = test_payload
         # function calls requests.get, need patch to get mock return value
