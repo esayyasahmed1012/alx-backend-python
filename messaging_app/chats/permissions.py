71195a7b6228fddafffe11eff2380f6cf76a22ef
esayyasahmed1012 <esayyasahmed1012@gmail.com>
2025-07-26 23:19:42 -0700

    .\permissions.py

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,12 +1,17 @@
 from rest_framework import permissions
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
-class IsParticipant(BasePermission):
+class IsParticipantOfConversation(BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
         return request.user == obj.sender or request.user == obj.recipient
 
 class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+        if request.method in SAFE_METHODS:
             return True
         return obj.owner == request.user
