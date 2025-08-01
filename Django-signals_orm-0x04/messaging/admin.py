508afca64860cd552535a77c8d2ea93bf4409fe4
esayyas <esayyasahmed1012@gmail.com>
2025-08-01 20:37:52 +0300

    admin.py

--- Django-signals_orm-0x04/messaging/admin.py
+++ Django-signals_orm-0x04/messaging/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
+
+class MessageHistoryInline(admin.TabularInline):
+    model = MessageHistory
+    extra = 0
+    readonly_fields = ['old_content', 'edited_at']
+    can_delete = False
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ['sender', 'receiver', 'content', 'timestamp']
-    list_filter = ['timestamp']
+    list_display = ['sender', 'receiver', 'content', 'timestamp', 'edited']
+    list_filter = ['timestamp', 'edited']
     search_fields = ['sender__username', 'receiver__username', 'content']
     date_hierarchy = 'timestamp'
+    inlines = [MessageHistoryInline]
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
@@ -14,3 +21,10 @@ class NotificationAdmin(admin.ModelAdmin):
     list_filter = ['is_read', 'created_at']
     search_fields = ['user__username']
     date_hierarchy = 'created_at'
+
+@admin.register(MessageHistory)
+class MessageHistoryAdmin(admin.ModelAdmin):
+    list_display = ['message', 'old_content', 'edited_at']
+    list_filter = ['edited_at']
+    search_fields = ['message__id', 'old_content']
+    date_hierarchy = 'edited_at'
