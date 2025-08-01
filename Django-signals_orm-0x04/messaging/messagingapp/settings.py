1f239f05fda78d4d5d51ceff2be12a43ef4fc70b
esayyas <esayyasahmed1012@gmail.com>
2025-08-01 21:02:26 +0300

    settings.py

---
+++ Django-signals_orm-0x04/messaging/messagingapp/settings.py
@@ -0,0 +1,9 @@
+# Other existing settings...
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
+
