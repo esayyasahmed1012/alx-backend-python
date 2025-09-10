5cae6c53b71a7129c11ebbab49bccb4bfbaef886
Esayyas Ahmed Yasin <EsayyasAhmed@cbe.com.et>
2025-09-10 15:29:15 +0300

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -12,6 +12,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, map, path, expected):
+       """"this is an exception the importd access_nested_map"""
        real_output=access_nested_map(map, path)
        self.assertEqual(real_output, expected)
     @parameterized.expand([
@@ -19,6 +20,7 @@ class TestAccessNestedMap(unittest.TestCase):
             ({"a": 1}, ("a", "b"), 1)
         ])
     def test_access_nested_map_exception(self, map, path, expected):
+        """this is a test for exception"""
         with self.assertRaises(KeyError) as e:
             access_nested_map(map, path)
             self.assertEqual(expected, e.exception)
